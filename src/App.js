bc66406dd9030ce0806481d8def2025df5382178
OzerK <ozerkerr@gmail.com>
2022-08-04 05:16:31 -0700

    intial set up and skeleton of app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Project from './components/Project';
+
+function App() {
+  return (
+    <div className="">
+      <Header />
+
+      <Project />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Project.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Project() {
+  return (
+    <div>
+      This is projcect  
+    </div>
+  )
+}
+
+export default Project;

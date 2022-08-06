526e951492bc9b5f02ebf8ede5c5c294b7499aef
OzerK <ozerkerr@gmail.com>
2022-08-06 03:52:08 -0700

    about me page

--- src/components/pages/AboutMe/AboutMe.jsx
+++ src/components/pages/AboutMe/AboutMe.jsx
@@ -1,10 +1,27 @@
 // import './App.css';
 import React from "react";
+import { Container, Image } from "react-bootstrap";
+import ProfilePicture from "../../../assets/images/ProfilePicture.JPG";
 
 const aboutMeText = `I have actively delivered smart cutting-edge user interfaces and successful production-ready web development code. With precision skill and craftsmanship, I have used my unique knowledge of design and front-end technologies to create cross-browser experiences focused on long-term user retention and engagement.`;
 
 function AboutMe() {
-  return <div>{aboutMeText}</div>;
+  return (
+    <Container>
+      <section className="my-3">
+        <h5>About Me</h5>
+      </section>
+      <section className="my-3">
+        <Image
+          src={ProfilePicture}
+          className="img-thumbnail"
+          // thumbnail={true}
+          rounded={true}
+        />
+      </section>
+      {aboutMeText}
+    </Container>
+  );
 }
 
 export default AboutMe;
